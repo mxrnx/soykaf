ae334f9b7584bec26f0d1edbd33e3e3fae89d678
knarka <knarka@users.noreply.github.com>
2017-01-25 21:41:01 +0100

    redirect after posts

--- submit.scm
+++ submit.scm
@@ -21,13 +21,14 @@
   (define name (if (null? (car postlist)) defname (car postlist)))
   (when (null? (cadr postlist)) (display "Comment empty, post discarded.") (exit 1))
   (when (not (string? (cadr postlist))) (display "Malformed request, post discarded.") (exit 1))
+  (when (null? (caddr postlist)) (display "Malformed request, post discarded") (exit 1))
   (define com (cadr postlist))
-  (display (con (string-append "insert into " table " (name, com) values ('" name "', '" com "')")))
+  (define reply (caddr postlist))
+  (display (con (string-append "insert into " table " (name, com, reply) values ('" name "', '" com "', '" reply"')")))
   #t)
 
 (if (string=? (get-environment-variable "REQUEST_METHOD") "POST")
   (begin
     (display "Content-Type: text/html\n\n")
-    (add-post (make-post-list (read-line (current-input-port))))
-    ))
+    (add-post (make-post-list (read-line (current-input-port))))))
 (redirect self)
