dcb76fc9cdaff7a5104c16457893db7292907a97
knarka <knarka@users.noreply.github.com>
2017-01-26 22:14:50 +0100

    stop default name ending up in the database

--- submit.scm
+++ submit.scm
@@ -18,13 +18,13 @@
        (string-split query "&")))
 
 (define (add-post postlist)
-  (define name (if (null? (car postlist)) defname (car postlist)))
+  (define name (if (null? (car postlist)) "NULL" (string-append "'" (car postlist) "'")))
   (when (null? (cadr postlist)) (display "Comment empty, post discarded.") (exit 1))
   (when (not (string? (cadr postlist))) (display "Malformed request, post discarded.") (exit 1))
   (when (null? (caddr postlist)) (display "Malformed request, post discarded") (exit 1))
   (define com (cadr postlist))
   (define reply (caddr postlist))
-  (display (con (string-append "insert into " table " (name, com, reply) values ('" name "', '" com "', '" reply"')")))
+  (display (con (string-append "insert into " table " (name, com, reply) values (" name ", '" com "', '" reply"')")))
   #t)
 
 (if (string=? (get-environment-variable "REQUEST_METHOD") "POST")
